ccc890de0a0fc370fb3babeddb8e8cade0c785f6
Marcio Maisonette <marciomaisonette@gmail.com>
2024-11-25 20:07:20 -0300

    chore: comments written in the js code converted to english

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// Cotação de moedas do dia.
+// Currency exchange rates.
 const USD = 4.87
 const EUR = 5.32
 const GBP = 6.08
 
-// Obtendo elementos do formulário.
+// Capturing form elements.
 const form = document.querySelector('form');
 const amount = document.getElementById('amount');
 const currency = document.getElementById('currency');
@@ -11,7 +11,7 @@ const footer = document.querySelector('main footer');
 const description = document.getElementById('description');
 const result = document.getElementById('result');
 
-// Manipulando o input amount para receber somente números.
+// Handling the 'input amount' to get only numbers.
 amount.addEventListener('input', () => {
 
 const hasCharactersRegex = /\D+/g;
@@ -19,7 +19,7 @@ amount.value = amount.value.replace(hasCharactersRegex, '');
 console.log(amount.value);  
 })
 
-// Capturando o evento de submit (enviar) do formulário.
+// Capturing submitted events from the form.
 form.onsubmit = (event) => {
   event.preventDefault()
 
@@ -36,42 +36,42 @@ form.onsubmit = (event) => {
   }
 }
 
-// Função para converter a moeda.
+// Converting currency function.
 function convertCurrency(amount, price, symbol){
   try {
 
-    // Exibindo a cotação da moeda selecionada.
+    // Showing the currency exchange rate from the selected currency.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
-    // Calcula o total.
+    // The total calculation.
     let total = amount * price
 
-    // Verifica se o resultado não é um número.
+    // Checking if the result isn't a number.
     if (isNaN(total)){
       return alert("Por favor, digite o valor corretamente para converter.")
     }
 
-    // Formatar o valor total.
+    // Format the total calculation.
     total = formatCurrencyBRL(total).replace("R$", "")
 
-    // Exibi o resultado total.
+    // Showing the total result.
     result.textContent = `${total} Reais`
 
-    // Aplica a classe que exibe o footer para mostrar como resultado.
+    // Apply the class responsible for showing the footer with the final result.
     footer.classList.add("show-result")
 
   } catch (error) {
     console.log(error)
 
-    // Remove a classe do footer removendo ele da tela.
+    // Remove the class responsible to show the footer.
     footer.classList.remove("show-result")
     alert("Não foi possível converter. Tente novamente mais tarde.")
   }
 }
 
-// Formata a moeda em Real Brasileiro.
+// Format the currency on Brazilian Real.
 function formatCurrencyBRL(value){
 
-  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00).
+  // Converting to number to be able to use the 'toLocaleString' function thus formating to BRL (R$ 00,00).
   return Number(value).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 }
